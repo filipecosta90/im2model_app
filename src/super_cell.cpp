6ac1196a378d4a04d239c179dc59ee761845f0cc
filipecosta90 <filipecosta.90@gmail.com>
2017-04-07 14:44:47 +0100

    going to test erase from container by indice for XY and Z out of range on supercell

--- src/super_cell.cpp
+++ src/super_cell.cpp
@@ -38,6 +38,16 @@
 // Parallel construction
 //#include <omp.h>
 
+  template<typename Cont, typename It>
+auto ToggleIndices(Cont &cont, It beg, It end) -> decltype(std::end(cont))
+{
+  int helpIndx(0);
+  return std::stable_partition(std::begin(cont), std::end(cont), 
+      [&](decltype(*std::begin(cont)) const& val) -> bool {
+      return std::find(beg, end, helpIndx++) == end;
+      });
+}
+
 Super_Cell::Super_Cell(){
   /** default and sentinel values **/
   set_sentinel_values();
