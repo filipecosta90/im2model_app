c64682cdda9f63cda4e82b86b43ea013f0125b48
Filipe Oliveira <filipecosta.90@gmail.com>
2017-04-29 16:27:09 +0100

    opencv is now fully working with .dll's on windows

--- src_win32/main.cxx
+++ src_win32/main.cxx
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv_modules.hpp"
+#include "opencv2/features2d/features2d.hpp"
 
 // Include the headers relevant to the boost::program_options
 // library
-#include <boost/program_options.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/tokenizer.hpp>
-#include <boost/token_functions.hpp>
-
-
+//#include "boost/program_options.hpp"
+//#include <boost/program_options/parsers.hpp>
+//#include <boost/program_options/variables_map.hpp>
+//#include <boost/tokenizer.hpp>
+//#include <boost/token_functions.hpp>
 
 int main (int argc, char *argv[])
 {
-    cv::Mat experimental_image;
-    std::cout << "win 32 test" << std::endl;
+    cv::Mat experimental_image(2,3,CV_8UC1);
+    std::cout << "win 32 test" << experimental_image << std::endl;
 
     return 0;
 }
