8866550f9648010f83a1b6b5cdd131477b513ee3
filipecosta90 <filipecosta.90@gmail.com>
2017-04-29 13:08:47 +0100

    working o msvc17 minimal example

---
+++ src_win32/main.cxx
@@ -0,0 +1,24 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+
+// Include the headers relevant to the boost::program_options
+// library
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
+
+
+
+int main (int argc, char *argv[])
+{
+    cv::Mat experimental_image;
+    std::cout << "win 32 test" << std::endl;
+
+    return 0;
+}
