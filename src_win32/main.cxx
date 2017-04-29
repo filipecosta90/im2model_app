1cc221e2d9fcd2f0ae2174ee77cad9f79c42ac27
Filipe Oliveira <filipecosta.90@gmail.com>
2017-04-29 20:12:14 +0100

    managed to boost and opencv working on windows

--- src_win32/main.cxx
+++ src_win32/main.cxx
@@ -4,6 +4,8 @@
 #include <math.h>
 
 #include <iostream>
+#include <string>
+
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -13,16 +15,39 @@
 
 // Include the headers relevant to the boost::program_options
 // library
-//#include "boost/program_options.hpp"
-//#include <boost/program_options/parsers.hpp>
+#include <boost/regex.hpp>
+#include "boost/tokenizer.hpp"
 //#include <boost/program_options/variables_map.hpp>
-//#include <boost/tokenizer.hpp>
-//#include <boost/token_functions.hpp>
+#define _HAS_AUTO_PTR_ETC 1
+#include "boost/token_functions.hpp"
+#define _HAS_AUTO_PTR_ETC 1
+#include <boost/program_options.hpp>
+#define _HAS_AUTO_PTR_ETC 1
+
+#include <boost/program_options/parsers.hpp>
+#define _HAS_AUTO_PTR_ETC 1
+#include <boost/program_options/variables_map.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
+
 
 int main (int argc, char *argv[])
 {
     cv::Mat experimental_image(2,3,CV_8UC1);
     std::cout << "win 32 test" << experimental_image << std::endl;
+
+
+	std::string line;
+	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+
+	while (std::cin)
+	{
+		std::getline(std::cin, line);
+		boost::smatch matches;
+		if (boost::regex_match(line, matches, pat))
+			std::cout << matches[2] << std::endl;
+	}
+
 
     return 0;
 }
