f5a47fffbea7db9188a2ada624ad32ac278cacb7
filipecosta90 <filipecosta.90@gmail.com>
2017-05-01 15:45:32 +0100

    celslc now fully using boost::process

--- src/celslc_prm.cpp
+++ src/celslc_prm.cpp
@@ -1,25 +1,23 @@
 #include "celslc_prm.hpp"
-#include <opencv2/core/hal/interface.h>        // for CV_64F
-#include <stdlib.h>                            // for EXIT_FAILURE, EXIT_SUC...
-#include <boost/process.hpp>
-#include <sys/_types/_s_ifmt.h>                // for S_IRUSR, S_IWUSR
-#include <sys/fcntl.h>                         // for open, O_CREAT, O_RDWR
-#include <sys/wait.h>                          // for waitpid, WEXITSTATUS
-#include <unistd.h>                            // for pid_t, _exit, execv, dup2
-#include <boost/filesystem/operations.hpp>     // for directory_iterator
-#include <boost/filesystem/path.hpp>           // for path, operator==, oper...
-#include <boost/iterator/iterator_facade.hpp>  // for iterator_facade_base
-#include <boost/thread.hpp>      // for thread
+
 #include <cassert>                             // for assert
 #include <cstdio>                              // for perror
 #include <iostream>                            // for operator<<, basic_ostream
 #include <fstream>
-#include <opencv2/core/mat.hpp>                // for Mat
-#include <opencv2/core/mat.inl.hpp>            // for Mat::Mat, Mat::at, Mat...
-#include <opencv2/core/types.hpp>              // for Point3d
 #include <string>                              // for allocator, char_traits
 #include <utility>                             // for pair
 #include <vector>                              // for vector, vector<>::iter...
+
+#include <boost/process.hpp>
+#include <boost/filesystem/operations.hpp>     // for directory_iterator
+#include <boost/filesystem/path.hpp>           // for path, operator==, oper...
+#include <boost/iterator/iterator_facade.hpp>  // for iterator_facade_base
+#include <boost/thread.hpp>      // for thread
+
+#include <opencv2/core/hal/interface.h>        // for CV_64F
+#include <opencv2/core/mat.hpp>                // for Mat
+#include <opencv2/core/mat.inl.hpp>            // for Mat::Mat, Mat::at, Mat...
+#include <opencv2/core/types.hpp>              // for Point3d
 
 static const std::string SLI_EXTENSION = ".sli";
 
@@ -350,7 +348,6 @@ bool CELSLC_prm::call_boost_bin(){
       <<  prp_dir_u << "," << prp_dir_v << "," << prp_dir_w << "," 
       << (float) super_cell_size_a << "," << (float) super_cell_size_b << "," << (float) super_cell_size_c;
   }
-
   /**  
    * Equidistant slicing of the super-cell along the c-axis. 
    * Specify an explicit number of slices, 
@@ -377,117 +374,10 @@ bool CELSLC_prm::call_boost_bin(){
     args_stream << " -abs";
   }
   boost::process::system( args_stream.str() ); 
-}
-
-bool CELSLC_prm::call_bin(){
-  pid_t pid;
-  std::vector<std::string> celslc_vector;
-  celslc_vector.push_back(  bin_path );
-
-  if( cif_format_switch  ){
-    celslc_vector.push_back("-cif");
-    celslc_vector.push_back( super_cell_cif_file );
+  if( auto_equidistant_slices_switch || auto_non_equidistant_slices_switch ){
+    update_nz_simulated_partitions_from_prm();
   }
-  else{
-    if( cel_format_switch  ){
-      celslc_vector.push_back("-cel");
-      celslc_vector.push_back(super_cell_cel_file );
-    }
-  }
-
-  celslc_vector.push_back("-slc");
-  celslc_vector.push_back(slc_file_name_prefix);
-
-  celslc_vector.push_back("-nx");
-  // input nx string
-  std::stringstream input_nx_stream;
-  input_nx_stream << nx_simulated_horizontal_samples;
-  celslc_vector.push_back( input_nx_stream.str() );
-
-  celslc_vector.push_back("-ny");
-  // input ny string
-  std::stringstream input_ny_stream;
-  input_ny_stream << ny_simulated_vertical_samples;
-  celslc_vector.push_back( input_ny_stream.str() );
-
-  celslc_vector.push_back("-ht");
-  // input ht
-  std::stringstream input_ht_stream;
-  input_ht_stream << ht_accelaration_voltage;
-  celslc_vector.push_back( input_ht_stream.str() );
-
-  if( projection_dir_hkl_switch && projected_dir_uvw_switch && super_cell_size_switch ){
-    celslc_vector.push_back("-prj");
-    std::stringstream input_prj_stream;
-    input_prj_stream << (float) prj_dir_h  << "," << (float) prj_dir_k << "," << (float) prj_dir_l << ","  
-      <<  prp_dir_u << "," << prp_dir_v << "," << prp_dir_w << "," 
-      << (float) super_cell_size_a << "," << (float) super_cell_size_b << "," << (float) super_cell_size_c;
-    celslc_vector.push_back(  input_prj_stream.str() );
-  }
-
-  /**  
-   * Equidistant slicing of the super-cell along the c-axis. 
-   * Specify an explicit number of slices, 
-   * or use -nz 0 to let CELSLC determine the number of equidistant slices automatically. 
-   * Omitting the -nz option will lead to an automatic non-equidistant slicing. 
-   * **/
-  if( !auto_non_equidistant_slices_switch ){
-    celslc_vector.push_back("-nz");
-    // input nz string
-    std::stringstream input_nz_stream;
-    if( auto_equidistant_slices_switch ){
-      //let CELSLC determine the number of equidistant slices automatically
-      input_nz_stream << "0"; 
-    }
-    else{
-      //Specify an explicit number of slices
-      input_nz_stream << nz_simulated_partitions;
-    }
-    celslc_vector.push_back( input_nz_stream.str() );
-  }
-
-  if ( dwf_switch ){
-    celslc_vector.push_back("-dwf");
-  }
-  if ( abs_switch ){
-    celslc_vector.push_back("-abs");
-  }
-  pid=fork();
-  if (pid  == -1) // system functions also set a variable called "errno"
-  {
-    perror("ERROR in vfork() of CELSLC call_bin"); // this function automatically checks "errno"
-    // and prints the error plus what you give it
-    return EXIT_FAILURE;
-  }
-  // ---- by when you get here there will be two processes
-  if (pid == 0) // child process
-  {
-    std::vector<const char*> argv;
-    argv.reserve(celslc_vector.size()+1);
-    for (auto& arg : celslc_vector) {
-      argv.push_back(arg.data());
-    }
-    argv.push_back(NULL);
-    execv(const_cast<char*>(argv[0]), const_cast<char**>(argv.data()));
-    perror("ERROR in execv of CELSLC call_bin"); // this function automatically checks "errno"
-    _exit(0);
-  }
-  else {
-    int status;
-    while (-1 == waitpid(pid, &status, 0));
-    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-      std::cerr << "Process (pid " << pid << ") failed" << std::endl;
-      exit(1);
-    }
-    std::cout << "Process with ID " << pid << " ended" << std::endl;
-    if( auto_equidistant_slices_switch || auto_non_equidistant_slices_switch ){
-      update_nz_simulated_partitions_from_prm();
-    }
-    runned_bin = true;
-    return EXIT_SUCCESS;
-  }
-  //if you get here something went wrong
-  return EXIT_FAILURE;
+  runned_bin = true;
 }
 
 bool CELSLC_prm::prepare_nz_simulated_partitions_from_ssc_prm(){
@@ -541,216 +431,103 @@ bool CELSLC_prm::prepare_nz_simulated_partitions_from_ssc_prm(){
 }
 
 bool CELSLC_prm::prepare_bin_ssc(){
-
-  int pid;
-  std::vector<std::string> celslc_vector;
-
-  celslc_vector.push_back( bin_path );
+  std::stringstream args_stream;
+  args_stream << bin_path;
 
   if( cif_format_switch  ){
-    celslc_vector.push_back( "-cif" );
-    celslc_vector.push_back( super_cell_cif_file );
+    args_stream << " -cif " << super_cell_cif_file;
   }
   else{
     if( cel_format_switch  ){
-      celslc_vector.push_back( "-cel" );
-      celslc_vector.push_back( super_cell_cel_file );
+      args_stream << " -cel " << super_cell_cel_file;
     }
   }
-
-  std::string slc_file_name_prefix_dummy = slc_file_name_prefix;
-  celslc_vector.push_back( "-slc" );
-  celslc_vector.push_back( slc_file_name_prefix_dummy );
-
-  celslc_vector.push_back( "-nx" );
+  args_stream << " -slc " << slc_file_name_prefix;
   // input nx string
-  celslc_vector.push_back( "32" );
-
-  celslc_vector.push_back( "-ny");
+  args_stream << " -nx 32";
   // input ny string
-  celslc_vector.push_back( "32" );
-
-  celslc_vector.push_back( "-ht" );
+  args_stream << " -ny 32"; 
   // input ht
-  std::stringstream input_ht_stream;
-  input_ht_stream << ht_accelaration_voltage;
-  celslc_vector.push_back( input_ht_stream.str() );
+  args_stream << " -ht " << ht_accelaration_voltage;
 
   if( projection_dir_hkl_switch && projected_dir_uvw_switch && super_cell_size_switch ){
-    celslc_vector.push_back( "-prj" );
-    std::stringstream input_prj_stream;
-    input_prj_stream 
-      << (float) prj_dir_h  << "," << (float) prj_dir_k << "," << (float) prj_dir_l << ","  
+    args_stream << " -prj " << (float) prj_dir_h  << "," << (float) prj_dir_k << "," << (float) prj_dir_l << ","  
       <<  prp_dir_u << "," << prp_dir_v << "," << prp_dir_w << "," 
       << (float) super_cell_size_a << "," << (float) super_cell_size_b << "," << (float) super_cell_size_c;
-    celslc_vector.push_back( input_prj_stream.str() );
   }
-
   /**  
    * Equidistant slicing of the super-cell along the c-axis. 
    * Specify an explicit number of slices, 
    * or use -nz 0 to let CELSLC determine the number of equidistant slices automatically. 
    * Omitting the -nz option will lead to an automatic non-equidistant slicing. 
    * **/
-  celslc_vector.push_back( "-nz" );
-  // input nz string
-  std::stringstream input_nz_stream;
-  //let CELSLC determine the number of equidistant slices automatically
-  input_nz_stream << "0";
-  celslc_vector.push_back( input_nz_stream.str() );
+  args_stream << " -nz 0";
 
   if ( dwf_switch ){
-    celslc_vector.push_back( "-dwf" );
+    args_stream << " -dwf";
   }
   if ( abs_switch ){
-    celslc_vector.push_back( "-abs" );
+    args_stream << " -abs";
   }
-
-  if ((pid = vfork()) == -1) // system functions also set a variable called "errno"
-  {
-    perror("ERROR in vfork() of CELSLC prepare_bin_ssc"); // this function automatically checks "errno"
-    // and prints the error plus what you give it
-    return EXIT_FAILURE;
-  }
-  // ---- by when you get here there will be two processes
-  if (pid == 0) // child process
-  {
-    std::vector<const char*> argv;
-    argv.reserve(celslc_vector.size());
-    for (auto& arg : celslc_vector) {
-      argv.push_back(arg.data());
-    }
-    argv.push_back(NULL);
-    execv(const_cast<char*>(argv[0]), const_cast<char**>(argv.data()));
-    perror("ERROR in execv of CELSLC call_bin"); // this function automatically checks "errno"
-    _exit(0);
-  }
-  else {
-    int status;
-    wait(&status);
-    prepare_nz_simulated_partitions_from_ssc_prm();
-    single_slice_calculation_prepare_bin_runned_switch = true;
-    std::cout << "ssc NZ max processes " << nz_simulated_partitions << std::endl;
-    return EXIT_SUCCESS;
-  }
-  //if you get here something went wrong
-  return EXIT_FAILURE;
+  boost::process::system( args_stream.str() ); 
+  prepare_nz_simulated_partitions_from_ssc_prm();
+  single_slice_calculation_prepare_bin_runned_switch = true;
+  std::cout << "ssc NZ max processes " << nz_simulated_partitions << std::endl;
 }
 
 bool CELSLC_prm::call_bin_ssc(){
   prepare_bin_ssc();
 
-  pid_t* pID = new pid_t[nz_simulated_partitions];
+  boost::process::group ssc_group;
+  std::stringstream args_stream;
+  args_stream << bin_path;
 
+  if( cif_format_switch  ){
+    args_stream << " -cif " << super_cell_cif_file;
+  }
+  else{
+    if( cel_format_switch  ){
+      args_stream << " -cel " << super_cell_cel_file;
+    }
+  }
+  args_stream << " -slc " << slc_file_name_prefix;
+  // input nx string
+  args_stream << " -nx " << nx_simulated_horizontal_samples;
+  // input ny string
+  args_stream << " -ny " << ny_simulated_vertical_samples;
+  
+  args_stream << " -nz " << nz_simulated_partitions;
+  // input ht
+  args_stream << " -ht " << ht_accelaration_voltage;
+
+  if( projection_dir_hkl_switch && projected_dir_uvw_switch && super_cell_size_switch ){
+    args_stream << " -prj " << (float) prj_dir_h  << "," << (float) prj_dir_k << "," << (float) prj_dir_l << ","  
+      <<  prp_dir_u << "," << prp_dir_v << "," << prp_dir_w << "," 
+      << (float) super_cell_size_a << "," << (float) super_cell_size_b << "," << (float) super_cell_size_c;
+  }
+
+
+  if ( dwf_switch ){
+    args_stream << " -dwf";
+  }
+  if ( abs_switch ){
+    args_stream << " -abs";
+  }
+  // input ssc
   for ( int slice_id = 1; slice_id <= nz_simulated_partitions; slice_id++ ){
-
-    std::vector<std::string> celslc_vector;
-
-    celslc_vector.push_back(   bin_path  );
-
-    if( cif_format_switch ){
-      celslc_vector.push_back( "-cif" );
-      celslc_vector.push_back( super_cell_cif_file );
-    }
-    else{
-      if( cel_format_switch  ){
-        celslc_vector.push_back( "-cel" );
-        celslc_vector.push_back( super_cell_cel_file );
-      }
-    }
-
-    celslc_vector.push_back( "-slc" );
-    celslc_vector.push_back( slc_file_name_prefix );
-
-    celslc_vector.push_back( "-nx" );
-    // input nx string
-    std::stringstream input_nx_stream;
-    input_nx_stream << nx_simulated_horizontal_samples;
-    celslc_vector.push_back( input_nx_stream.str() );
-
-    celslc_vector.push_back( "-ny" );
-    // input ny string
-    std::stringstream input_ny_stream;
-    input_ny_stream << ny_simulated_vertical_samples;
-    celslc_vector.push_back( input_ny_stream.str() );
-
-    /**  
-     * Equidistant slicing of the super-cell along the c-axis. 
-     * Specify an explicit number of slices, 
-     * **/
-    celslc_vector.push_back( "-nz" );
-    // input nz string
-    std::stringstream input_nz_stream;
-    input_nz_stream << nz_simulated_partitions;
-    celslc_vector.push_back( input_nz_stream.str() );
-
-    celslc_vector.push_back( "-ht" );
-    // input ht
-    std::stringstream input_ht_stream;
-    input_ht_stream << ht_accelaration_voltage;
-    celslc_vector.push_back( input_ht_stream.str() );
-
-    if ( dwf_switch ){
-      celslc_vector.push_back("-dwf");
-    }
-    if ( abs_switch ){
-      celslc_vector.push_back("-abs");
-    }
-    if( projection_dir_hkl_switch && projected_dir_uvw_switch && super_cell_size_switch ){
-      celslc_vector.push_back("-prj");
-      std::stringstream input_prj_stream;
-      input_prj_stream 
-        << (float) prj_dir_h  << "," << (float) prj_dir_k << "," << (float) prj_dir_l << ","  
-        <<  prp_dir_u << "," << prp_dir_v << "," << prp_dir_w << "," 
-        << (float) super_cell_size_a << "," << (float) super_cell_size_b << "," << (float) super_cell_size_c;
-      celslc_vector.push_back( input_prj_stream.str() );
-    }
-
-    celslc_vector.push_back("-ssc");
-    // input ssc
-    std::stringstream input_ssc_stream;
-    input_ssc_stream << slice_id; 
-    celslc_vector.push_back( input_ssc_stream.str() );
-
-    pID[slice_id-1]=fork();
-    if (pID[slice_id-1] == -1) // system functions also set a variable called "errno"
-    {
-      perror("ERROR in vfork() of CELSLC call_bin"); // this function automatically checks "errno"
-      // and prints the error plus what you give it
-      return EXIT_FAILURE;
-    }
-    if (pID[slice_id-1] == 0) // child process
-    {
-      std::cout << "Process for slice #" << slice_id << std::endl;
-      std::stringstream celslc_stream;
-      celslc_stream << "log_" << slc_file_name_prefix << "_" << slice_id << ".log"; 
-      std::cout << "Saving log of slice #"<< slice_id << " in file: " << celslc_stream.str() << std::endl ;
-      int fd = open( celslc_stream.str().c_str()  , O_RDWR | O_CREAT, S_IRUSR | S_IWUSR );
-      dup2(fd, 1);   // make stdout go to file
-      dup2(fd, 2);   // make stderr go to file - you may choose to not do this
-      close(fd);     // fd no longer needed - the dup'ed handles are sufficient
-      std::vector<const char*> argv;
-      argv.reserve(celslc_vector.size());
-      for (auto& arg : celslc_vector) {
-        argv.push_back(arg.data());
-      }
-      argv.push_back(NULL);
-      execv(const_cast<char*>(argv[0]), const_cast<char**>(argv.data()));
-      perror("ERROR in execv of CELSLC call_bin"); // this function automatically checks "errno"
-      _exit(0);
-    }
+    std::stringstream ssc_stream;
+    ssc_stream << args_stream.str() << " -ssc " << slice_id;
+    std::cout << "Process for slice #" << slice_id << std::endl;
+    std::cout << "\tcommand: " << ssc_stream.str() << std::endl;
+    std::stringstream celslc_stream;
+    celslc_stream << "log_" << slc_file_name_prefix << "_" << slice_id << ".log"; 
+    std::cout << "Saving log of slice #"<< slice_id << " in file: " << celslc_stream.str() << std::endl ;
+    boost::process::spawn( ssc_stream.str(), ssc_group ); 
   }
-  for (int i = 0; i < nz_simulated_partitions; ++i) {
-    int status;
-    while (-1 == waitpid(pID[i], &status, 0));
-    if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-      std::cerr << "Process " << i << " (pid " << pID[i] << ") failed" << std::endl;
-      exit(1);
-    }
-    std::cout << "Process with ID " << pID[i] << " ended" << std::endl;
-  }
+  
+  ssc_group.wait();
   ssc_runned_bin = true; 
-  return EXIT_SUCCESS;
+  return true; 
 }
 
 
