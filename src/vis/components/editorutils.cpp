4386f59672cd8cbc7620a65154ee2aa5d4574d44
filipecosta90 <filipecosta.90@gmail.com>
2017-08-21 17:06:10 +0100

    corrected qrc loading

--- src/vis/components/editorutils.cpp
+++ src/vis/components/editorutils.cpp
@@ -28,80 +28,11 @@
 #include "editorutils.h"
 #include "qdummyobjectpicker.h"
 
-#include <Qt3DCore/QEntity>
-#include <Qt3DRender/QCamera>
-#include <Qt3DRender/QCameraLens>
-#include <Qt3DRender/QAbstractTexture>
-#include <Qt3DRender/QTextureImage>
-#include <Qt3DRender/QObjectPicker>
-#include <Qt3DRender/QMaterial>
-#include <Qt3DRender/QEffect>
-#include <Qt3DRender/QParameter>
-#include <Qt3DRender/QTechnique>
-#include <Qt3DRender/QFilterKey>
-#include <Qt3DRender/QRenderPass>
-#include <Qt3DRender/QRenderState>
-#include <Qt3DRender/QTexture>
-#include <Qt3DRender/QAbstractTextureImage>
-#include <Qt3DRender/QSceneLoader>
-#include <Qt3DCore/QTransform>
-
-#include <Qt3DRender/QMesh>
-#include <Qt3DExtras/QCuboidMesh>
-#include <Qt3DExtras/QCylinderMesh>
-#include <Qt3DExtras/QPlaneMesh>
-#include <Qt3DExtras/QSphereMesh>
-#include <Qt3DExtras/QTorusMesh>
-
-#include <Qt3DExtras/QDiffuseMapMaterial>
-#include <Qt3DExtras/QDiffuseSpecularMapMaterial>
-#include <Qt3DExtras/QGoochMaterial>
-#include <Qt3DExtras/QNormalDiffuseMapMaterial>
-#include <Qt3DExtras/QNormalDiffuseMapAlphaMaterial>
-#include <Qt3DExtras/QNormalDiffuseSpecularMapMaterial>
-#include <Qt3DExtras/QPerVertexColorMaterial>
-#include <Qt3DExtras/QPhongAlphaMaterial>
-#include <Qt3DExtras/QPhongMaterial>
-
-#include <Qt3DRender/QAbstractLight>
-#include <Qt3DRender/QDirectionalLight>
-#include <Qt3DRender/QPointLight>
-#include <Qt3DRender/QSpotLight>
-
-#include <Qt3DRender/QBuffer>
-#include <Qt3DRender/QBufferDataGenerator>
-#include <Qt3DRender/QAttribute>
-#include <Qt3DRender/QGeometryFactory>
-
-#include <Qt3DRender/QAlphaCoverage>
-#include <Qt3DRender/QAlphaTest>
-#include <Qt3DRender/QBlendEquation>
-#include <Qt3DRender/QBlendEquationArguments>
-#include <Qt3DRender/QColorMask>
-#include <Qt3DRender/QCullFace>
-#include <Qt3DRender/QMultiSampleAntiAliasing>
-#include <Qt3DRender/QNoDepthMask>
-#include <Qt3DRender/QDepthTest>
-#include <Qt3DRender/QDithering>
-#include <Qt3DRender/QFrontFace>
-#include <Qt3DRender/QPointSize>
-#include <Qt3DRender/QPolygonOffset>
-#include <Qt3DRender/QScissorTest>
-#include <Qt3DRender/QStencilTest>
-#include <Qt3DRender/QStencilTestArguments>
-#include <Qt3DRender/QStencilMask>
-#include <Qt3DRender/QStencilOperation>
-#include <Qt3DRender/QStencilOperationArguments>
-#include <Qt3DRender/QClipPlane>
-#include <Qt3DRender/QSeamlessCubemap>
-
-#include <iostream>
-#include <QtCore/QtMath>
-
 // Creates a single arrow
-Qt3DRender::QGeometryRenderer *EditorUtils::createArrowMesh(){
+Qt3DRender::QMesh *EditorUtils::createArrowMesh(){
   Qt3DRender::QMesh *customMesh = new Qt3DRender::QMesh();
-  QUrl source( QStringLiteral("qrc:/MeshArrow3d") );
+  //QUrl source( QStringLiteral(":/MeshArrow3d") );
+  QUrl source ("qrc:///Icons/MeshArrow3d");
   if (!source.isValid()) {
           std::cout << "Invalid URL: " << source.toString().toStdString() << std::endl;
       }
--- src/vis/components/editorutils.h
+++ src/vis/components/editorutils.h
@@ -28,9 +28,80 @@
 #ifndef EDITORUTILS_H
 #define EDITORUTILS_H
 
+
+#include <Qt3DCore/QEntity>
+#include <Qt3DRender/QCamera>
+#include <Qt3DRender/QCameraLens>
+#include <Qt3DRender/QAbstractTexture>
+#include <Qt3DRender/QTextureImage>
+#include <Qt3DRender/QObjectPicker>
+#include <Qt3DRender/QMaterial>
+#include <Qt3DRender/QEffect>
+#include <Qt3DRender/QParameter>
+#include <Qt3DRender/QTechnique>
+#include <Qt3DRender/QFilterKey>
+#include <Qt3DRender/QRenderPass>
+#include <Qt3DRender/QRenderState>
+#include <Qt3DRender/QTexture>
+#include <Qt3DRender/QAbstractTextureImage>
+#include <Qt3DRender/QSceneLoader>
+#include <Qt3DCore/QTransform>
+
+#include <Qt3DRender/QMesh>
+#include <Qt3DExtras/QCuboidMesh>
+#include <Qt3DExtras/QCylinderMesh>
+#include <Qt3DExtras/QPlaneMesh>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DExtras/QTorusMesh>
+
+#include <Qt3DExtras/QDiffuseMapMaterial>
+#include <Qt3DExtras/QDiffuseSpecularMapMaterial>
+#include <Qt3DExtras/QGoochMaterial>
+#include <Qt3DExtras/QNormalDiffuseMapMaterial>
+#include <Qt3DExtras/QNormalDiffuseMapAlphaMaterial>
+#include <Qt3DExtras/QNormalDiffuseSpecularMapMaterial>
+#include <Qt3DExtras/QPerVertexColorMaterial>
+#include <Qt3DExtras/QPhongAlphaMaterial>
+#include <Qt3DExtras/QPhongMaterial>
+
+#include <Qt3DRender/QAbstractLight>
+#include <Qt3DRender/QDirectionalLight>
+#include <Qt3DRender/QPointLight>
+#include <Qt3DRender/QSpotLight>
+
+#include <Qt3DRender/QBuffer>
+#include <Qt3DRender/QBufferDataGenerator>
+#include <Qt3DRender/QAttribute>
+#include <Qt3DRender/QGeometryFactory>
+
+#include <Qt3DRender/QAlphaCoverage>
+#include <Qt3DRender/QAlphaTest>
+#include <Qt3DRender/QBlendEquation>
+#include <Qt3DRender/QBlendEquationArguments>
+#include <Qt3DRender/QColorMask>
+#include <Qt3DRender/QCullFace>
+#include <Qt3DRender/QMultiSampleAntiAliasing>
+#include <Qt3DRender/QNoDepthMask>
+#include <Qt3DRender/QDepthTest>
+#include <Qt3DRender/QDithering>
+#include <Qt3DRender/QFrontFace>
+#include <Qt3DRender/QPointSize>
+#include <Qt3DRender/QPolygonOffset>
+#include <Qt3DRender/QScissorTest>
+#include <Qt3DRender/QStencilTest>
+#include <Qt3DRender/QStencilTestArguments>
+#include <Qt3DRender/QStencilMask>
+#include <Qt3DRender/QStencilOperation>
+#include <Qt3DRender/QStencilOperationArguments>
+#include <Qt3DRender/QClipPlane>
+#include <Qt3DRender/QSeamlessCubemap>
+
+#include <iostream>
+#include <QtCore/QtMath>
 #include <QtCore/QObject>
 #include <QtGui/QMatrix4x4>
 #include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DRender/QMesh>
 
 namespace Qt3DCore {
   class QEntity;
@@ -55,9 +126,9 @@ class EditorUtils : public QObject
   Q_OBJECT
   public:
 
-    static Qt3DRender::QGeometryRenderer *createArrowMesh();
+    static Qt3DRender::QMesh *createArrowMesh();
     static Qt3DCore::QEntity *createArrowEntity(const QColor &color, Qt3DCore::QEntity *parent, const QMatrix4x4 &matrix, const QString &name);
-static void removeExpandedChildEntities(Qt3DCore::QEntity *entity, const QString &childName );
+    static void removeExpandedChildEntities(Qt3DCore::QEntity *entity, const QString &childName );
     static Qt3DRender::QObjectPicker *entityPicker(Qt3DCore::QEntity *entity);
     static void setEnabledToSubtree(Qt3DCore::QEntity *entity, bool enable);
     static Qt3DCore::QEntity *findEntityByName(Qt3DCore::QEntity *entity, const QString &name);
